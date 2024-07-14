7246fb6c3ca679f3a19e26ec9b3b99b823751c37
ADNova Design <160766366+ADNova-Design@users.noreply.github.com>
2024-07-14 01:18:11 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,35 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('service-worker.js')
-        .then(function(registration) {
-          console.log('Service Worker registrado con éxito:', registration);
-        })
-        .catch(function(error) {
-          console.log('Error al registrar el Service Worker:', error);
-        });
-    });
-  }
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function(registration) {
+        console.log('Service Worker registrado con éxito:', registration);
+      })
+      .catch(function(error) {
+        console.log('Error al registrar el Service Worker:', error);
+      });
+  });
+}
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-app-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/icon.png',
+        '/icon512.png'
+        // Agrega aquí los archivos que deseas cachear
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});

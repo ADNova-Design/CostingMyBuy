84646d1dd22353937825216b79d5668656c88ed8
ADNova Design <160766366+ADNova-Design@users.noreply.github.com>
2024-08-11 00:11:04 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,33 @@
+
+const CACHE_NAME = 'calculadora-cache-v1';
+const urlsToCache = [
+  '/',
+  '/calc/css/style.css',
+  '/calc/js/script.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('service-worker.js')
-        .then(function(registration) {
-          console.log('Service Worker registrado con éxito:', registration);
-        })
-        .catch(function(error) {
-          console.log('Error al registrar el Service Worker:', error);
-        });
-    });
-  }
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function(registration) {
+        console.log('Service Worker registrado con éxito:', registration);
+      })
+      .catch(function(error) {
+        console.log('Error al registrar el Service Worker:', error);
+      });
+  });
+}

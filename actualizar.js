7fe20739a6b30bd0a86d2639c1f4089832e6d3a2
ADNova Design <160766366+ADNova-Design@users.noreply.github.com>
2024-08-20 02:05:20 -0400

    Delete actualizar.js

--- actualizar.js
+++
@@ -1,6 +0,0 @@
-<script>
-document.getElementById('actualizar').addEventListener('click', function() {
-  // Redirigir a la URL deseada al hacer clic en el botón
-  window.location.href = 'https://drive.google.com/drive/folders/1ULbST64eWCBEI4i-NK-0tiDvaqpNs9Gx?usp=sharing';
-});
-</script>

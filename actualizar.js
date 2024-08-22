7b47491eeeb3317973eb2f473dc576cca6313712
ADNova Design <160766366+ADNova-Design@users.noreply.github.com>
2024-08-22 00:08:45 -0400

    Delete actualizar.js

--- actualizar.js
+++
@@ -1,6 +0,0 @@
-
-// FUNCION ACTUALIZAR APP
-document.getElementById('actualizar').addEventListener('click', function() {
-  // Redirigir a la URL deseada al hacer clic en el botón
-  window.location.href = 'https://github.com/ADNova-Design/CostingMyBuy/blob/main/Costing%20My%20Buy%20AppV1.apk';
-});
